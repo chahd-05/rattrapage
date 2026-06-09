0c61d98cbb5747b00fba9b0d0551a2e9bb1d3c74
chahed mazouz <mazouzchahd12@gmail.com>
2026-06-09 18:26:42 +0100

    feat: implement Book model with fillable fields and reservations relationship

---
+++ app/Models/Book.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\Reservation;
+use Illuminate\Database\Eloquent\Model;
+
+class Book extends Model
+{
+    protected $fillable = [
+        'title',
+        'author',
+        'description',
+        'status'
+    ];
+    public function reservations()
+{
+    return $this->hasMany(Reservation::class);
+}
+}
