6b6092f2b233645b795b522e341cd873e255082b
chahed mazouz <mazouzchahd12@gmail.com>
2026-06-09 18:50:35 +0100

    feat: add loans relationship to User model

--- app/Models/User.php
+++ app/Models/User.php
@@ -30,6 +30,11 @@ class User extends Authenticatable
     return $this->hasMany(Reservation::class);
 }
 
+public function loans()
+{
+    return $this->hasMany(Loan::class);
+}
+
     /**
      * The attributes that should be hidden for serialization.
      *
